0a68c31eed22a150e426ec7ad8a34a8dc0edd4f7
verstandinvictus <peter.k.cawley@gmail.com>
2015-10-06 15:41:20 -0700

    Now with more parsing!

--- tpsreport.py
+++ tpsreport.py
@@ -1,13 +1,22 @@
 from simple_salesforce import Salesforce
 import config
+import collections
+import json
 
-sf = Salesforce (username=config.username,
-                 password=config.password,
-                 security_token=config.security_token)
+sf = Salesforce(username=config.username,
+                password=config.password,
+                security_token=config.security_token)
 
 testresults = sf.query(
-    "SELECT CaseNumber from Case where OwnerId = "
+    "SELECT CaseNumber, (SELECT Field, OldValue, NewValue from Histories WHERE Field = 'Status') from Case where OwnerId = "
     + config.Maverick
     + " AND status = 'resolving internally'")
 
-print testresults
+jsonresults = json.dumps(testresults, sort_keys=True, indent=4, separators=(',',': '))
+#print jsonresults
+
+parsedresults = json.loads(jsonresults)
+for record in parsedresults["records"]:
+    print record["CaseNumber"]
+    for x in record["Histories"]["records"]:
+        print x["NewValue"]
