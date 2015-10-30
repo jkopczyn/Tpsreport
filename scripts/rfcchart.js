37274873c3fd3fe4eac680ea46763ba232f6c512
VerstandInvictus <peter.k.cawley@gmail.com>
2015-10-30 03:16:35 -0700

    mouseover legend item shows appropriate data label!

--- scripts/rfcchart.js
+++ scripts/rfcchart.js
@@ -2,29 +2,42 @@ var colors = ['#104060','#125b94','#1f7fc1','#67add8']
 
 $.getJSON('scripts/testdata.json', function(data) {
     var rfcchart = c3.generate({
-    bindto: '#rfcchart',
-    data: {
-        rows: data.rows,
-        type: 'bar',
-        groups: [data.groups],
-        order:null
-        },
-    color: {
-        pattern: colors
-        },
-    axis: {
-        rotated: true,
-        x: {
-            type: 'category',
-            categories: data.categories
+        bindto: '#rfcchart',
+        data: {
+            rows: data.rows,
+            type: 'bar',
+            groups: [data.groups],
+            order:null,
+            labels: true,
             },
-        y: {
-            show: false
+        color: {
+            pattern: colors
+            },
+        axis: {
+            rotated: true,
+            x: {
+                type: 'category',
+                categories: data.categories
+                },
+            y: {
+                show: false
+                }
+            },
+        legend: {
+            position: 'bottom',
+            item: {
+                onmouseover: function (d) {
+                        var d2 = d.replace(/ /g, '-')
+                        var whatlabel = ".c3-texts-" + d2 + ' text';
+                        $(whatlabel).css("display", "inline")
+                    },
+                onmouseout: function (d) {
+                        var d2 = d.replace(/ /g, '-')
+                        var whatlabel = ".c3-texts-" + d2 + ' text';
+                        $(whatlabel).css("display", "none")
+                    }
+                }
             }
-        },
-    legend: {
-        position: 'bottom',
-        }
-    });
-})
-
+        })
+    }
+);
